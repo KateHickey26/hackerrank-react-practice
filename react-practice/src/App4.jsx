f91779ecdd23daf11f9838a75fabcf3434a22e87
KateHickey26 <kate.hickey26@gmail.com>
2025-10-14 15:30:26 +1100

    add practice file

---
+++ react-practice/src/App4.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState} from 'react';
+import axios from 'axios';
+
+function App4() {
+    const [posts, setPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    //use effect to get posts 
+    useEffect(() => {   
+        setIsLoading(true);
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                setPosts(response.data);
+            })
+            .catch(error => {
+                console.error('Error : ', error);
+            })
+            .finally(() => setIsLoading(false));
+
+    }, []);
+
+    return (
+        <div style={{ padding: 16 }}>
+            <h1>Posts</h1>
+            {isLoading ? (
+                <p>Loading posts...</p>
+            ) : (
+                <ul>
+                    {posts.map(post => (
+                        <li key={post.id} style={{ marginBottom: 8 }}>
+                            <strong>{post.title}</strong>
+                            <p>{post.body}</p>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    )
+}
+
+export default App4;
